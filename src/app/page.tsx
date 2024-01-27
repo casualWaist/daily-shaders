8d38c8369d8c097aa2af25f075015af33f37699b
Matthew <119624750+casualWaist@users.noreply.github.com>
2024-01-26 22:19:39 -0500

    2024-01-27

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,6 @@
 export default function Home() {
 
     return <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1>Choose a date to see that day's shader</h1>
+        <h1 className="">Choose a date to see that day's shader</h1>
     </main>
 }
