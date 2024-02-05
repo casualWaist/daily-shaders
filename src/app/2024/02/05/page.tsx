46526401acce3e44ac35ddd50c06662cb84c5b84
Matthew <119624750+casualWaist@users.noreply.github.com>
2024-02-04 22:42:53 -0500

    2024-02-05

---
+++ src/app/2024/02/05/page.tsx
@@ -0,0 +1,62 @@
+'use client'
+
+import {Canvas, MaterialNode} from "@react-three/fiber"
+import * as THREE from 'three'
+import { extend, useFrame } from '@react-three/fiber'
+import {Float, Html, OrbitControls, shaderMaterial} from '@react-three/drei'
+import vertex from './vertex.glsl'
+import fragment from './fragment.glsl'
+import { forwardRef, useImperativeHandle, useRef } from 'react'
+import {Caveat} from "next/font/google"
+
+const caveat = Caveat({
+    subsets: ['latin'],
+    variable: '--font-caveat'
+})
+
+const MarchRot = shaderMaterial({
+        time: 0,
+        color: new THREE.Color(0.5, 0.0, 0.025),
+        // this ternary is necessary because SSR
+        resolution: typeof window !== 'undefined' ? new THREE.Vector2(window.innerWidth, window.innerHeight) : new THREE.Vector2(1, 1),
+    },
+    vertex,
+    fragment,
+)
+
+type Props = MaterialNode<any, any> & { time?: number, color?: THREE.Color, resolution?: THREE.Vector2 }
+extend({ MarchRot })
+declare module "@react-three/fiber" {
+    interface ThreeElements {
+        marchRot: MaterialNode<any, any>
+    }
+}
+
+const Shader = forwardRef(({ ...props }: Props, ref) => {
+    const localRef = useRef<THREE.ShaderMaterial & {time: number, color:THREE.Color, resolution?: THREE.Vector2}>(null!)
+
+    useImperativeHandle(ref, () => localRef.current)
+
+    useFrame((_, delta) => (localRef.current.time += delta))
+    return <marchRot key={MarchRot.key} ref={localRef} {...props} attach='material' />
+})
+Shader.displayName = 'Shader'
+
+export default function Page() {
+    return <Canvas>
+        <mesh position={[0, 1, 0]}>
+            <boxGeometry args={[2.5, 2.5, 2.5]} />
+            {/* @ts-ignore */}
+            <Shader time={0} color={'grey'}/>
+        </mesh>
+        <OrbitControls />
+        <Float>
+            <Html position={[0, -1.5, 1]}
+                  center transform as="h1" className={caveat.className} scale={0.25}>
+                <div style={{ transform: 'scale(4)', textAlign: 'center' }}>
+                    The Floor is<br/>Ribbons
+                </div>
+            </Html>
+        </Float>
+    </Canvas>
+}
