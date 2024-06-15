612266cc87412f6edbcbb9b97472d543890ab3da
Matthew <119624750+casualWaist@users.noreply.github.com>
2024-06-14 23:13:15 -0400

    2024-06-14

---
+++ src/app/2024/06/12/vertex.glsl
@@ -0,0 +1,20 @@
+varying vec2 vUv;
+varying vec3 vPosition;
+varying float vDistance;
+varying float vSize;
+uniform float uFocus;
+uniform vec2 uResolution;
+uniform sampler2D uPositions;
+uniform float uTime;
+uniform vec3 uCamera;
+
+// #particlesInGeometry #vertexShape
+void main() {
+    vUv = uv;
+    vec4 pos = texture(uPositions, uv);
+    vec4 mvPosition = modelViewMatrix * pos;
+    gl_Position = projectionMatrix * mvPosition;
+    vDistance = abs(uFocus - -mvPosition.z);
+    vSize = (step(-0.5, uv.x)) * vDistance * 10.1;
+    gl_PointSize = vSize;
+}
