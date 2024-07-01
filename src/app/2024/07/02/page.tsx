6769f52f873c63fec4c8caa4dd765fd11edb2b2c
Matthew <119624750+casualWaist@users.noreply.github.com>
2024-07-01 09:00:28 -0400

    2024-07-02

---
+++ src/app/2024/07/02/page.tsx
@@ -0,0 +1,117 @@
+'use client'
+
+import {Canvas, extend, MaterialNode, useFrame, useThree} from "@react-three/fiber"
+import {Float, Html, shaderMaterial, OrbitControls, useTexture} from "@react-three/drei"
+import { Caveat } from "next/font/google"
+import fragment from './fragment.glsl'
+import vertex from './vertex.glsl'
+import * as THREE from 'three'
+import {forwardRef, useImperativeHandle, useRef} from "react"
+
+const caveat = Caveat({
+    subsets: ['latin'],
+    variable: '--font-caveat'
+})
+
+const VolumeCycleShaderImp = shaderMaterial({
+    uTime: 0,
+    uTexture: new THREE.Texture(),
+    uColor: new THREE.Color(0xeeeeee),
+    uInvModelMatrix: new THREE.Matrix4(),
+    uScale: new THREE.Vector3( 2, 2, 2 ),
+    uSeed: Math.random() * 19.19,
+    uNoiseScale: new THREE.Vector4(1.75, 1.75, 1.0, 0.25),
+    uCameraPos: new THREE.Vector3(),
+    uMagnitude: 0.7,
+    uLacunarity: 1.5,
+    uGain: 0.9,
+    uResolution: typeof window !== 'undefined' ? new THREE.Vector2(window.innerWidth, window.innerHeight) : new THREE.Vector2(1, 1),
+}, vertex, fragment)
+
+extend({ VolumeCycleShaderImp })
+
+declare global {
+    namespace JSX {
+        interface IntrinsicElements {
+            volumeCycleShaderImp: MaterialNode<any, typeof THREE.MeshStandardMaterial>
+        }
+    }
+}
+
+type Props = {
+    uTime: number,
+    uColor: THREE.Color,
+    uTexture: THREE.Texture,
+    uInvModelMatrix?: THREE.Matrix4,
+    uScale?: THREE.Vector3,
+    uSeed?: number,
+    uNoiseScale?: THREE.Vector4,
+    uCameraPos?: THREE.Vector3,
+    uMagnitude?: number,
+    uLacunarity?: number,
+    uGain?: number,
+    uResolution?: THREE.Vector2
+} & JSX.IntrinsicElements['meshStandardMaterial']
+
+const VolumeCycleShader = forwardRef(({...props}: Props, ref) => {
+    const localRef = useRef<Props>(null!)
+    useImperativeHandle(ref, () => ({ shaderImp: localRef.current }))
+    useFrame((state, delta) => {
+        localRef.current.uTime += delta
+        localRef.current.uCameraPos = state.camera.position
+    })
+    return <volumeCycleShaderImp key={VolumeCycleShaderImp.key} ref={localRef} attach="material" {...props} />
+})
+VolumeCycleShader.displayName = 'VolumeCycleShader'
+
+export default function Page() {
+    return <Canvas style={{position: "absolute", top: "0", zIndex: "-1", backgroundColor: "#000"}}>
+        <Scene />
+        <Float>
+            <Html position={[0, -1.5, 1]}
+                center
+                transform
+                as="h1"
+                className={caveat.className}
+                scale={0.25}>
+                <div style={{transform: 'scale(4)', color: "#181615"}}>
+                    Because 3D
+                </div>
+            </Html>
+        </Float>
+        <OrbitControls />
+    </Canvas>
+}
+
+function Scene() {
+    const view = useThree((state) => state.viewport)
+    const texture = useTexture('/Fire.png')
+    texture.magFilter = texture.minFilter = THREE.LinearFilter
+    texture.wrapS = texture.wrapT = THREE.ClampToEdgeWrapping
+    const meshRef = useRef<THREE.Mesh>(null!)
+    const shaderRef = useRef<MaterialNode<any, typeof THREE.MeshStandardMaterial>>(null!)
+    const boxRef = useRef<THREE.Mesh>(null!)
+
+    useFrame((state, delta) => {
+        boxRef.current.rotation.y -= delta * 0.5
+    })
+
+    return <>
+        <mesh ref={meshRef}>
+            <boxGeometry args={[view.width * 0.25, view.width * 0.25, view.width * 0.25]}/>
+            <VolumeCycleShader ref={shaderRef}
+                               uTime={0}
+                               uTexture={texture}
+                               transparent
+                               depthTest={false}
+                               depthWrite={false}
+                               uColor={new THREE.Color(0.75, 0.75, 0.75)}/>
+        </mesh>
+        <mesh ref={boxRef} position={[0, -1.5, 0]}>
+            <boxGeometry args={[view.width * 0.25, view.width * 0.0625, view.width * 0.25]}/>
+            <meshStandardMaterial color={"#423907"} />
+        </mesh>
+        <directionalLight intensity={20} position={[-1, 1, 0.2]}/>
+        <directionalLight intensity={10} position={[1, 1, 0.4]}/>
+    </>
+}
---
+++ src/app/2024/07/02/vertex.glsl
@@ -0,0 +1,18 @@
+uniform float uTime;
+varying vec2 vUv;
+varying vec3 vPosition;
+varying vec3 vWorldPos;
+void main() {
+    vUv = uv;
+    vPosition = position;
+    float cos = cos(uTime * 0.5);
+    float sin = sin(uTime * 0.5);
+    mat4 rotationMatrix = mat4(
+        cos, 0.0, sin, 0.0,
+        0.0, 1.0, 0.0, 0.0,
+        -sin, 0.0, cos, 0.0,
+        0.0, 0.0, 0.0, 1.0
+    );
+    vWorldPos = (rotationMatrix * modelMatrix * vec4(position, 1.0)).xyz;
+    gl_Position = projectionMatrix * viewMatrix * rotationMatrix * modelMatrix * vec4(position, 1.0);
+}
